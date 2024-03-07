51f386104c0202bec292830fbbf1d05ec7453905
callmepvp <mrxgameing@gmail.com>
2024-03-07 23:49:20 +0200

    added constants class

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,9 @@
 public class Car {
+    //Main Variables
     private double velocity;
     private double acceleration;
+    private double totalMass;
+
+    //Secondary Variables
+    private double centrifugalForce;
 }
---
+++ src/Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+    //All variable constants go here
+    public static final double GRAVITATIONAL_CONSTANT = 9.818;
+}
