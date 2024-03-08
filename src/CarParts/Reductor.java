b0133426ed8e58ec572657757718b7d10334ced5
Siim Kasemets <siim.kasemets@ut.ee>
2024-03-08 11:42:43 +0200

    Added a few variables to the Reductor class with getter/setters.

--- src/CarParts/Reductor.java
+++ src/CarParts/Reductor.java
@@ -1,9 +1,63 @@
 package CarParts;
 
 public class Reductor extends CarPart {
+    // Variables
+    private double[] gearRatios;
+    private int currentGear;
+    private int maxGear;
+    private int revLimit;
+    private double reductorEfficiency;
+
     //Constructor
-    public Reductor(double mass) {
+    public Reductor(double mass, double[] gearRatios, int currentGear, int maxGear, int revLimit, double reductorEfficiency) {
         super(mass);
+        this.gearRatios = gearRatios;
+        this.currentGear = currentGear;
+        this.maxGear = maxGear;
+        this.revLimit = revLimit;
+        this.reductorEfficiency = reductorEfficiency;
+    }
+    // Getter/Setters
+
+
+    public double[] getGearRatios() {
+        return gearRatios;
+    }
+
+    public void setGearRatios(double[] gearRatios) {
+        this.gearRatios = gearRatios;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public int getMaxGear() {
+        return maxGear;
+    }
+
+    public void setMaxGear(int maxGear) {
+        this.maxGear = maxGear;
+    }
+
+    public int getRevLimit() {
+        return revLimit;
+    }
+
+    public void setRevLimit(int revLimit) {
+        this.revLimit = revLimit;
+    }
+
+    public double getReductorEfficiency() {
+        return reductorEfficiency;
+    }
+
+    public void setReductorEfficiency(double reductorEfficiency) {
+        this.reductorEfficiency = reductorEfficiency;
     }
 
     //Main Methods
