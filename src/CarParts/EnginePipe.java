c3763eb2945468591ae6497e897afc676d1bce6f
taavi200 <52968766+taavi200@users.noreply.github.com>
2024-03-29 14:33:02 +0200

    Update EnginePipe.java

--- src/CarParts/EnginePipe.java
+++ src/CarParts/EnginePipe.java
@@ -1,14 +1,28 @@
-package CarParts;
+public class EnginePipe {
+    private double mass; //kg
+    private double pikkus; //võiks olla 0-40 vahel, kus 20 on perfektne
 
-public class EnginePipe extends CarPart {
-    //Constructor
-    public EnginePipe(double mass) {
-        super(mass);
+    public Toru(double pikkus) {
+        this.pikkus = pikkus;
+        mass=pikkus/100;
     }
 
-    //Main Methods
-    @Override
     public double getMass() {
         return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getPikkus() {
+        return pikkus;
+    }
+
+    public void setPikkus(double pikkus) {
+        this.pikkus = pikkus;
+    }
+    public double kiirendusekoeffitsent(){
+        return (((-3/4)*pikkus+30*pikkus-20)/280);
     }
 }
